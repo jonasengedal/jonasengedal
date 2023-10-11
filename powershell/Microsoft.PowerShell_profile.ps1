c63b10c432af10fe9ba2f0d2261c6d336dfc442f
Jonas Nygaard Engedal <jen@cvation.com>
2023-10-11 10:22:13 +0200

    Added dockerpostgres alias

--- powershell/Microsoft.PowerShell_profile.ps1
+++ powershell/Microsoft.PowerShell_profile.ps1
@@ -13,3 +13,15 @@ Function gitmergemainfunc { $b = git branch --show-current; git checkout main; g
 Set-Alias -Name gitmergemain -Value gitmergemainfunc -Scope Global
 Function gitrebasemainfunc { $b = git branch --show-current; git branch "$b-backup-$(Get-Date -f "HHmmss")"; $c = git stash create; git reset --hard; git checkout main; git pull; git checkout $b; git rebase main; if ($c) { git stash apply $c } }
 Set-Alias -Name gitrebasemain -Value gitrebasemainfunc -Scope Global
+Function dockerpostgresfunc { 
+    $cN = "local-postgres"; 
+    $c = docker ps -a | Select-String $cN; 
+    if (-not $c) { 
+        docker run --name $cN -e POSTGRES_PASSWORD=Local123 -p 5432:5432 -d postgres 
+    }
+    else { 
+        docker start $cN
+    }
+    docker ps -a | Select-String $cN
+}
+Set-Alias -Name dockerpostgres -Value dockerpostgresfunc -Scope Global
