3a9d5f2f885a4bc8441ce06305705de396ad72ca
Jonas Nygaard Engedal <jen@cvation.com>
2024-01-16 15:19:03 +0100

    More git command powershell functions

--- powershell/Microsoft.PowerShell_profile.ps1
+++ powershell/Microsoft.PowerShell_profile.ps1
@@ -5,15 +5,89 @@ Import-Module oh-my-posh
 Set-PoshPrompt -Theme paradox
 
 # Create aliases for common git commands
-Function gitmainfunc { git checkout main; git pull }
-Set-Alias -Name gitmain -Value gitmainfunc -Scope Global
-Function gitpushoriginfunc { $b = git branch --show-current; git push origin -u $b }
-Set-Alias -Name gitpushorigin -Value gitpushoriginfunc -Scope Global
-Function gitmergemainfunc { $b = git branch --show-current; git checkout main; git pull; git checkout $b; git merge main }
-Set-Alias -Name gitmergemain -Value gitmergemainfunc -Scope Global
-Function gitrebasemainfunc { $b = git branch --show-current; git branch "$b-backup-$(Get-Date -f "HHmmss")"; $c = git stash create; git reset --hard; git checkout main; git pull; git checkout $b; git rebase main; if ($c) { git stash apply $c } }
-Set-Alias -Name gitrebasemain -Value gitrebasemainfunc -Scope Global
-Function dockerpostgresfunc { 
+function gcheckout { 
+    [CmdletBinding()]
+    param (
+        [Parameter(Position = 1, Mandatory, ValueFromPipeline, ValueFromPipelineByPropertyName)]
+        [string]$Branch
+    )
+    PROCESS {
+        git checkout main
+        git pull
+    }
+}
+function gco { 
+    [CmdletBinding()]
+    param (
+        [Parameter(Position = 1, Mandatory, ValueFromPipeline, ValueFromPipelineByPropertyName)]
+        [string]$Branch
+    )
+    PROCESS {
+        gcheckout $Branch
+    }
+}
+
+function gresethard {
+    [CmdletBinding(SupportsShouldProcess,
+        ConfirmImpact = 'High')]
+    param (
+        [Parameter(Position = 1, ValueFromPipeline, ValueFromPipelineByPropertyName)]
+        [string]$Commit = "HEAD^"
+    )
+    PROCESS {
+        if ($PSCmdlet.ShouldProcess('RESET HARD')) {
+            git reset --hard $Commit
+        }    
+    }
+}
+function grh {
+    [CmdletBinding()]
+    param (
+        [Parameter(Position = 1, ValueFromPipeline, ValueFromPipelineByPropertyName)]
+        [string]$Commit
+    )
+    PROCESS {
+        gresethard $Commit
+    }
+}
+
+function gresetsoft {
+    [CmdletBinding(SupportsShouldProcess,
+        ConfirmImpact = 'High')]
+    param (
+        [Parameter(Position = 1, ValueFromPipeline, ValueFromPipelineByPropertyName)]
+        [string]$Commit = "HEAD^"
+    )
+    PROCESS {
+        if ($PSCmdlet.ShouldProcess('RESET HARD')) {
+            git reset --soft $Commit
+        }    
+    }
+}
+function grs {
+    [CmdletBinding()]
+    param (
+        [Parameter(Position = 1, ValueFromPipeline, ValueFromPipelineByPropertyName)]
+        [string]$Commit
+    )
+    PROCESS {
+        gresetsoft $Commit
+    }
+}
+
+function gmain { git checkout main; git pull }
+Set-Alias -Name gmain -Value gmain -Scope Global
+function gpull { git pull }
+Set-Alias -Name gpl -Value gpull -Scope Global
+function gpush { git push }
+Set-Alias -Name gph -Value gpush -Scope Global
+function gpushorigin { $b = git branch --show-current; git push origin -u $b }
+Set-Alias -Name gpo -Value gpushorigin -Scope Global
+function gmergemain { $b = git branch --show-current; git checkout main; git pull; git checkout $b; git merge main }
+Set-Alias -Name gmm -Value gmergemain -Scope Global
+function grebasemain { $b = git branch --show-current; git branch "$b-backup-$(Get-Date -f "HHmmss")"; $c = git stash create; git reset --hard; git checkout main; git pull; git checkout $b; git rebase main; if ($c) { git stash apply $c } }
+Set-Alias -Name grm -Value grebasemain -Scope Global
+function dockerpostgres { 
     $cN = "local-postgres"; 
     $c = docker ps -a | Select-String $cN; 
     if (-not $c) { 
@@ -24,4 +98,4 @@ Function dockerpostgresfunc {
     }
     docker ps -a | Select-String $cN
 }
-Set-Alias -Name dockerpostgres -Value dockerpostgresfunc -Scope Global
+Set-Alias -Name dpostgres -Value dockerpostgres -Scope Global
