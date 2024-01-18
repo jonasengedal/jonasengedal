20cc7a9ccad0b15eab51b0e41a3c5d95e1bb9c18
Jonas Nygaard Engedal <jen@cvation.com>
2024-01-18 13:31:31 +0100

    Backup powershell profile file

--- powershell/Deploy.Powershell_profile.ps1
+++ powershell/Deploy.Powershell_profile.ps1
@@ -1,1 +1,6 @@
+# Backup existing Powershell profile file
+New-Item -Path "$(Split-Path -parent $profile)/Backup" -ItemType Directory -ErrorAction SilentlyContinue
+Copy-Item $profile -Destination "$(Split-Path -parent $profile)/Backup/$([DateTime]::UtcNow.ToString('yyyyMMddHHmmss'))_Microsoft.Powershell_profile.ps1"
+
+# Deploy Powershell profile file
 Copy-Item ./Microsoft.Powershell_profile.ps1 -Destination $profile
