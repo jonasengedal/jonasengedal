d8c05c5b62504050c8490b5db306caa4a0bdaa87
Jonas Nygaard Engedal <jen@cvation.com>
2024-02-07 09:27:31 +0100

    Tools manager script

--- tools/choco-install.ps1
+++
@@ -1,2 +0,0 @@
-choco install k9s
-choco install kubernetes-cli
---
+++ tools/tools-manager.ps1
@@ -0,0 +1,51 @@
+param(
+    [Parameter(Position = 0, Mandatory = $false)]
+    [bool]$Install = $false
+)
+
+$chocoApps = @('k9s', 'kubernetes-cli')
+
+$wingetApps = @(
+    'dbeaver.dbeaver',
+    'Git.Git',
+    'Google.Chrome',
+    'Graphviz.Graphviz',
+    'Hashicorp.Terraform',
+    'Hashicorp.Vault',
+    'Insomnia.Insomnia',
+    'JanDeDobbeleer.OhMyPosh',
+    'Microsoft.Azure.StorageExplorer',
+    'Microsoft.DotNet.SDK.3_1',
+    'Microsoft.DotNet.SDK.6',
+    'Microsoft.DotNet.SDK.7',
+    'Microsoft.DotNet.SDK.8',
+    'Microsoft.DotNet.AspNetCore.6',
+    'Microsoft.AzureCLI',
+    'Microsoft.VisualStudio.2022.Enterprise',
+    'Microsoft.VisualStudioCode',
+    'Mozilla.Firefox',
+    'Microsoft.PowerShell',
+    'Microsoft.PowerToys',
+    'Python.Python.3.10',
+    'ShareX.ShareX')
+
+if($Install){
+    foreach ($app in $chocoApps) {
+        Write-Host "Choco install $app"
+        choco install $app
+    }
+    foreach ($app in $wingetApps) {
+        Write-Host "Winget install $app"
+        winget install --id=$app --exact
+    }
+}
+else {
+    foreach ($app in $chocoApps) {
+        Write-Host "Choco upgrade $app"
+        choco upgrade $app --confirm
+    }
+    foreach ($app in $wingetApps) {
+        Write-Host "Winget upgrade $app"
+        winget upgrade --id=$app --exact --silent
+    }
+}
